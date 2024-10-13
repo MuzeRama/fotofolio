c1ff6eea6bc9e356a1334b7bc8921e983efed020
zein <mamduhzein4678@gmail.com>
2024-10-13 14:22:55 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/', function () {
+    return view('profil');
+});
+
+Route::get('/', function () {
+    return view('about');
+});
+
+Route::get('/', function () {
+    return view('skill');
+});
+
+Route::get('/', function () {
+    return view('project');
+});
